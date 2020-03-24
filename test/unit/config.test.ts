e02d81cb1a39c8beb6ab9da6ad48f6fe30d4b03a
WMXPY <wm@mengw.io>
2020-03-24 11:35:40 -0500

    finish config unit tests

--- test/unit/config.test.ts
+++ test/unit/config.test.ts
@@ -7,7 +7,7 @@
 
 import { expect } from "chai";
 import * as Chance from "chance";
-import { fixHexColor } from "../../src";
+import { fixAlpha, fixHexColor } from "../../src";
 
 describe('Given [Config] Helper Methods', (): void => {
 
@@ -21,6 +21,48 @@ describe('Given [Config] Helper Methods', (): void => {
             const result: number = fixHexColor(source);
 
             expect(result).to.be.equal(source);
+        });
+
+        it('should be able to fix multiple precision number', (): void => {
+
+            const source: number = chance.natural({ min: 0, max: 255 });
+            // tslint:disable-next-line: no-magic-numbers
+            const result: number = fixHexColor(source + 0.5);
+
+            expect(result).to.be.equal(source);
+        });
+
+        it('should be able to fix overflow number', (): void => {
+
+            const source: number = chance.natural({ min: 0, max: 255 });
+            // tslint:disable-next-line: no-magic-numbers
+            const resultOver: number = fixHexColor(source + 255);
+            const resultDown: number = fixHexColor(-source);
+
+            // tslint:disable-next-line: no-magic-numbers
+            expect(resultOver).to.be.equal(255);
+            expect(resultDown).to.be.equal(0);
+        });
+    });
+
+    describe('Given [fixAlpha] Function', (): void => {
+
+        it('should be able to fix normal alpha', (): void => {
+
+            const source: number = chance.floating({ min: 0, max: 1 });
+            const result: number = fixAlpha(source);
+
+            expect(result).to.be.equal(parseFloat(result.toFixed(2)));
+        });
+
+        it('should be able to fix overflow alpha', (): void => {
+
+            const source: number = parseFloat(chance.floating({ min: 0, max: 1 }).toFixed(2));
+            const resultOver: number = fixHexColor(source + 1);
+            const resultDown: number = fixHexColor(-source);
+
+            expect(resultOver).to.be.equal(1);
+            expect(resultDown).to.be.equal(0);
         });
     });
 });
